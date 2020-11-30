8844b84ccb10929de60efb61baf01097363187dc
Swetha Chandrasekhar <swethac68@gmail.com>
2020-11-30 15:08:19 -0500

    Restructured file organization

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header.js'
-import './App.scss';
+import '../styles/App.scss';
 import Inventory from './Inventory.js';
-import "./fontawesome";
+import "./fontawesome.js";
 
 class App extends Component {
   render() {
